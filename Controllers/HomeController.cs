a539961e036f5f0180ad9e4242f8d10f44b90c4c
AtilaEzequiel <112121260+AtilaEzequiel@users.noreply.github.com>
2023-06-09 17:13:11 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,272 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WaffleWebWorksTpFinal.Models;
+// usibg
+using Microsoft.Data.SqlClient;
+
+namespace WaffleWebWorksTpFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MovieADO;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        List<Carrera> list = new List<Carrera>();
+
+        public IActionResult Index()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                string queryString = "Select * from Carrera";
+                //string queryString = "INSERT INTO MovieADO (Id, titulo, fecha, genero, precio) VALUES (10, 'Delta', 15/12/1999, 'magia', 600);";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //  command.ExecuteReader(queryString);
+                //command.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Carrera movieADOs = new Carrera()
+                    {
+                        Id = int.Parse(reader[0].ToString()),
+                        Name = reader[1].ToString(),
+                        //
+                        //ReleaseDate = DateTime.Parse(reader[2].ToString()),
+                        Description = reader[2].ToString(),
+                        //  Price = int.Parse(reader[4].ToString()),
+                    };
+                    list.Add(movieADOs);
+                    // return View(movieADOs);
+
+                    //  list.Add(movieADOs);
+                    //list.Add(movieADOs1);
+                }
+
+
+
+                connection.Close();
+                return View(list);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Carrera mov)
+        {
+            try
+            {
+
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                connection.Open();
+
+                string queryString = "INSERT INTO Carrera (carrera, Descripcion) VALUES ( @Carrera, @Description);";
+                //string queryString = "INSERT INTO MovieADO (Id, titulo, fecha, genero, precio) VALUES (10, 'Delta', 15/12/1999, 'magia', 600);";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //  command.ExecuteReader(queryString);
+                command.Parameters.AddWithValue("@Carrera", mov.Name);
+                command.Parameters.AddWithValue("@Description", mov.Description);
+                
+
+
+
+
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                connection.Close();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public ActionResult Detalle(int id)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                string queryString = "Select * from carrera Where Id=@ID";
+                //string queryString = "INSERT INTO carrera (Id, titulo, fecha, genero, precio) VALUES (10, 'Delta', 15/12/1999, 'magia', 600);";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //  command.ExecuteReader(queryString);
+                command.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Carrera movieADOs = new Carrera()
+                    {
+                        Id = int.Parse(reader[0].ToString()),
+                        Name = reader[1].ToString(),
+                        //
+                        //ReleaseDate = DateTime.Parse(reader[2].ToString()),
+                        Description = reader[2].ToString(),
+                    };
+                    return View(movieADOs);
+
+                    //  list.Add(movieADOs);
+                    //list.Add(movieADOs1);
+                }
+
+
+
+                connection.Close();
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Detalle(int id, Carrera mov)
+        {
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                string queryString = "update carrera   set  carrera=@Name, Descripcion=@Description  where Id = @id";
+                //string queryString = "INSERT INTO MovieADO (Id, titulo, fecha, genero, precio) VALUES (10, 'Delta', 15/12/1999, 'magia', 600);";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //  command.ExecuteReader(queryString);
+                command.Parameters.AddWithValue("@Id", id);
+                command.Parameters.AddWithValue("@Name", mov.Name);
+                command.Parameters.AddWithValue("@Description", mov.Description);
+                
+
+
+
+
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                connection.Close();
+
+                return RedirectToAction("Detalle");
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                string queryString = "Select * from carrera Where Id=@ID";
+                //string queryString = "INSERT INTO carrera (Id, titulo, fecha, genero, precio) VALUES (10, 'Delta', 15/12/1999, 'magia', 600);";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //  command.ExecuteReader(queryString);
+                command.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Carrera movieADOs = new Carrera()
+                    {
+                        Id = int.Parse(reader[0].ToString()),
+                        Name = reader[1].ToString(),
+                        //
+                        //ReleaseDate = DateTime.Parse(reader[2].ToString()),
+                        Description = reader[2].ToString(),
+                    };
+                    return View(movieADOs);
+
+                    //  list.Add(movieADOs);
+                    //list.Add(movieADOs1);
+                }
+
+
+
+                connection.Close();
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete( Carrera mov)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                string queryString = "delete from carrera where Id = @Id";
+                //string queryString = "INSERT INTO MovieADO (Id, titulo, fecha, genero, precio) VALUES (10, 'Delta', 15/12/1999, 'magia', 600);";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //  command.ExecuteReader(queryString);
+                command.Parameters.AddWithValue("@Id", mov.Id);
+
+
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                connection.Close();
+
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
